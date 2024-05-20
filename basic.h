920edc5b8eecefc5509c503e59d220f73220818e
gab462 <gustavoaraujob1+1@gmail.com>
2024-05-20 17:30:50 -0300

    Change indentation

--- basic.h
+++ basic.h
@@ -21,9 +21,9 @@ typedef float f32;
 typedef double f64;
 
 typedef struct {
-  u64 size;
-  u64 position;
-  max_align_t memory;
+    u64 size;
+    u64 position;
+    max_align_t memory;
 } Arena;
 
 Arena* make_arena(u32 size);
@@ -33,9 +33,9 @@ void* allocate_impl(Arena* arena, u32 size, u32 alignment);
 #define allocate_n(arena, T, n) allocate_impl(arena, sizeof(T) * n, alignof(T))
 
 typedef struct {
-  u64 length;
-  u64 append;
-  max_align_t data;
+    u64 length;
+    u64 append;
+    max_align_t data;
 } Vector;
 
 void* make_vector_impl(Arena* arena, u32 type_size, u32 length);
@@ -46,8 +46,8 @@ Vector* get_vector(void* v);
 #define foreach(T, v) for (T* it = v; (u64){ it - v } < get_vector(v)->length; ++it)
 
 typedef struct {
-  char* data;
-  u64 length;
+    char* data;
+    u64 length;
 } String;
 
 // TODO: fmt
@@ -73,181 +73,181 @@ void println(String string);
 #include <unistd.h>
 
 Arena* make_arena(u32 size) {
-  Arena* arena = malloc(sizeof(Arena) - sizeof(max_align_t) + size);
+    Arena* arena = malloc(sizeof(Arena) - sizeof(max_align_t) + size);
 
-  arena->size = size;
-  arena->position = 0;
+    arena->size = size;
+    arena->position = 0;
 
-  return arena;
+    return arena;
 }
 
 void* allocate_impl(Arena* arena, u32 size, u32 alignment) {
-  arena->position += arena->position % alignment;
+    arena->position += arena->position % alignment;
 
-  assert(arena->position + size <= arena->size);
+    assert(arena->position + size <= arena->size);
 
-  void* address = &arena->memory + arena->position;
+    void* address = &arena->memory + arena->position;
 
-  arena->position += size;
+    arena->position += size;
 
-  return address;
+    return address;
 }
 
 void* make_vector_impl(Arena* arena, u32 type_size, u32 length) {
-  Vector* vector =
-    allocate_impl(arena,
-                  sizeof(Vector) - sizeof(max_align_t) + type_size * length,
-                  alignof(Vector));
+    Vector* vector =
+        allocate_impl(arena,
+                      sizeof(Vector) - sizeof(max_align_t) + type_size * length,
+                      alignof(Vector));
 
-  vector->length = length;
-  vector->append = 0;
+    vector->length = length;
+    vector->append = 0;
 
-  return &vector->data;
+    return &vector->data;
 }
 
 Vector* get_vector(void* v) {
-  return (void*){ (char*){ v } - offsetof(Vector, data) };
+    return (void*){ (char*){ v } - offsetof(Vector, data) };
 }
 
 String read_file(Arena* arena, String file) {
-  Arena* scratch = make_arena(255);
+    Arena* scratch = make_arena(255);
 
-  char* file_cstr = cstr(scratch, file);
+    char* file_cstr = cstr(scratch, file);
 
-  FILE* f = fopen(file_cstr, "rb");
+    FILE* f = fopen(file_cstr, "rb");
 
-  free(scratch);
+    free(scratch);
 
-  assert(f);
+    assert(f);
 
-  assert(fseek(f, 0, SEEK_END) >= 0);
+    assert(fseek(f, 0, SEEK_END) >= 0);
 
-  String string = {
-    .length = ftell(f)
-  };
+    String string = {
+        .length = ftell(f)
+    };
 
-  assert(fseek(f, 0, SEEK_SET) >= 0);
+    assert(fseek(f, 0, SEEK_SET) >= 0);
 
-  string.data = allocate_n(arena, char, string.length);
+    string.data = allocate_n(arena, char, string.length);
 
-  fread(string.data, 1, string.length, f);
+    fread(string.data, 1, string.length, f);
 
-  assert(ferror(f) == 0);
+    assert(ferror(f) == 0);
 
-  fclose(f);
+    fclose(f);
 
-  return string;
+    return string;
 }
 
 String append_string(Arena* arena, String this, String other) {
-  String string = {
-    .length = this.length + other.length
-  };
+    String string = {
+        .length = this.length + other.length
+    };
 
-  string.data = allocate_n(arena, char, string.length);
+    string.data = allocate_n(arena, char, string.length);
 
-  memcpy(string.data, this.data, this.length);
-  memcpy(string.data + this.length, other.data, other.length);
+    memcpy(string.data, this.data, this.length);
+    memcpy(string.data + this.length, other.data, other.length);
 
-  return string;
+    return string;
 }
 
 String* split_string(Arena* arena, String string, char separator) {
-  u32 separator_count = count_characters(string, separator);
-  String* strings = make_vector(arena, String, separator_count + 1);
+    u32 separator_count = count_characters(string, separator);
+    String* strings = make_vector(arena, String, separator_count + 1);
 
-  u64 position = 0;
+    u64 position = 0;
 
-  for (u64 i = 0; i < string.length; ++i) {
-    if (string.data[i] == separator) {
-      append(strings, ((String){ string.data + position, i - position }));
-      position = i + 1;
+    for (u64 i = 0; i < string.length; ++i) {
+        if (string.data[i] == separator) {
+            append(strings, ((String){ string.data + position, i - position }));
+            position = i + 1;
+        }
+
+        if (i + 1 == string.length)
+            append(strings, ((String){ string.data + position, string.length - position }));
     }
 
-    if (i + 1 == string.length)
-      append(strings, ((String){ string.data + position, string.length - position }));
-  }
-
-  return strings;
+    return strings;
 }
 
 String join_strings(Arena* arena, String separator, String* strings) {
-  String string = {
-    .length = 0
-  };
+    String string = {
+        .length = 0
+    };
 
-  foreach(String, strings) {
-    string.length += it->length + separator.length;
-  }
-
-  string.length -= separator.length;
-
-  string.data = allocate_n(arena, char, string.length);
-
-  u64 position = 0;
-
-  foreach(String, strings) {
-    memcpy(string.data + position, it->data, it->length);
-    position += it->length;
-
-    if (position < string.length) {
-      // Also copy separator
-      memcpy(string.data + position, separator.data, separator.length);
-      position += separator.length;
+    foreach(String, strings) {
+        string.length += it->length + separator.length;
     }
-  }
 
-  return string;
+    string.length -= separator.length;
+
+    string.data = allocate_n(arena, char, string.length);
+
+    u64 position = 0;
+
+    foreach(String, strings) {
+        memcpy(string.data + position, it->data, it->length);
+        position += it->length;
+
+        if (position < string.length) {
+            // Also copy separator
+            memcpy(string.data + position, separator.data, separator.length);
+            position += separator.length;
+        }
+    }
+
+    return string;
 }
 
 bool string_equals(String this, String other) {
-  return memcmp(this.data, other.data, this.length) == 0;
+    return memcmp(this.data, other.data, this.length) == 0;
 }
 
 bool substring(String this, String other) {
-  if (other.length > this.length)
-    return false;
+    if (other.length > this.length)
+        return false;
 
-  u64 match = 0;
+    u64 match = 0;
 
-  for (u64 i = 0; i < this.length; ++i) {
-    if (this.data[i] == other.data[match]) {
-      ++match;
-    } else {
-      match = this.data[i] == other.data[0] ? 1 : 0;
+    for (u64 i = 0; i < this.length; ++i) {
+        if (this.data[i] == other.data[match]) {
+            ++match;
+        } else {
+            match = this.data[i] == other.data[0] ? 1 : 0;
+        }
+
+        if (match == other.length)
+            return true;
     }
 
-    if (match == other.length)
-      return true;
-  }
-
-  return false;
+    return false;
 }
 
 u32 count_characters(String string, char character) {
-  u32 count = 0;
+    u32 count = 0;
 
-  for (u64 i = 0; i < string.length; ++i) {
-    if (string.data[i] == character)
-      ++count;
-  }
+    for (u64 i = 0; i < string.length; ++i) {
+        if (string.data[i] == character)
+            ++count;
+    }
 
-  return count;
+    return count;
 }
 
 char* cstr(Arena* arena, String string) {
-  char* new = allocate_n(arena, char, string.length + 1);
+    char* new = allocate_n(arena, char, string.length + 1);
 
-  memcpy(new, string.data, string.length);
+    memcpy(new, string.data, string.length);
 
-  new[string.length] = '\0';
+    new[string.length] = '\0';
 
-  return new;
+    return new;
 }
 
 void println(String string) {
-  assert(write(STDOUT_FILENO, string.data, string.length) >= 0);
-  assert(write(STDOUT_FILENO, "\n", 1) >= 0);
+    assert(write(STDOUT_FILENO, string.data, string.length) >= 0);
+    assert(write(STDOUT_FILENO, "\n", 1) >= 0);
 }
 
 #endif
--- example.c
+++ example.c
@@ -4,69 +4,69 @@
 #include "basic.h"
 
 int main(void) {
-  Arena* arena = make_arena(16);
+    Arena* arena = make_arena(16);
 
-  int* n = allocate(arena, int);
+    int* n = allocate(arena, int);
 
-  double* d = allocate(arena, double);
+    double* d = allocate(arena, double);
 
-  *n = 4;
+    *n = 4;
 
-  *d = 12.0;
+    *d = 12.0;
 
-  printf("Hello, World! %d %.2f\n", *n, *d);
+    printf("Hello, World! %d %.2f\n", *n, *d);
 
-  free(arena);
+    free(arena);
 
-  arena = make_arena(64);
+    arena = make_arena(64);
 
-  int* vec = make_vector(arena, int, 4);
+    int* vec = make_vector(arena, int, 4);
 
-  for (int i = 0; i < 4; ++i) {
-    vec[i] = i;
-  }
+    for (int i = 0; i < 4; ++i) {
+        vec[i] = i;
+    }
 
-  foreach(int, vec) {
-    printf("%d\n", *it);
-  }
+    foreach(int, vec) {
+        printf("%d\n", *it);
+    }
 
-  int* vec2 = make_vector(arena, int, 4);
+    int* vec2 = make_vector(arena, int, 4);
 
-  append(vec2, 5);
-  append(vec2, 6);
-  append(vec2, 7);
-  append(vec2, 8);
+    append(vec2, 5);
+    append(vec2, 6);
+    append(vec2, 7);
+    append(vec2, 8);
 
-  foreach(int, vec2) {
-    printf("%d\n", *it);
-  }
+    foreach(int, vec2) {
+        printf("%d\n", *it);
+    }
 
-  free(arena);
+    free(arena);
 
-  arena = make_arena(2048);
+    arena = make_arena(2048);
 
-  //println(read_file(arena, s("main.c")));
+    //println(read_file(arena, s("main.c")));
 
-  println(append_string(arena, s("Hello,"), s("World!")));
+    println(append_string(arena, s("Hello,"), s("World!")));
 
-  String* strings = make_vector(arena, String, 3);
-  append(strings, s("Hello"));
-  append(strings, s("World!"));
-  append(strings, s("Test!"));
+    String* strings = make_vector(arena, String, 3);
+    append(strings, s("Hello"));
+    append(strings, s("World!"));
+    append(strings, s("Test!"));
 
-  println(join_strings(arena, s(", "), strings));
+    println(join_strings(arena, s(", "), strings));
 
-  String* split = split_string(arena, s("Hello,World,Test"), ',');
+    String* split = split_string(arena, s("Hello,World,Test"), ',');
 
-  foreach(String, split) {
-    println(*it);
-  }
+    foreach(String, split) {
+        println(*it);
+    }
 
-  free(arena);
+    free(arena);
 
-  printf("Contains: %d\n", substring(s("Hello World"), s("Hello")));
-  printf("Contains: %d\n", substring(s("Hello World"), s("World")));
-  printf("Contains: %d\n", substring(s("Hello World"), s("Word")));
+    printf("Contains: %d\n", substring(s("Hello World"), s("Hello")));
+    printf("Contains: %d\n", substring(s("Hello World"), s("World")));
+    printf("Contains: %d\n", substring(s("Hello World"), s("Word")));
 
-  return 0;
+    return 0;
 }
